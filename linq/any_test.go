2508f151713f0749801be3ea8a26320f6c4e28a1
Mason Ellis <mason.ellis95@gmail.com>
2022-05-18 21:13:25 -0500

    Refactored int tests and added float and string tests.

--- linq/any_test.go
+++ linq/any_test.go
@@ -7,79 +7,155 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Any_Int_EmptySlice_ReturnsFalse(t *testing.T) {
-	s := make([]int, 0)
+func Test_Any_Int(t *testing.T) {
+	testCases := []struct {
+		name string
+		s    []int
+		want bool
+	}{
+		{
+			name: "Int, Empty Slice, Returns False",
+			s:    make([]int, 0),
+			want: false,
+		},
+		{
+			name: "Int, No Valid Items, Returns False",
+			s:    []int{1, 2, 3},
+			want: false,
+		},
+		{
+			name: "Int, Valid Item at Start, Returns True",
+			s:    []int{0, 1, 2, 3},
+			want: true,
+		},
+		{
+			name: "Int, Valid Item at End, Returns True",
+			s:    []int{1, 2, 3, 0},
+			want: true,
+		},
+		{
+			name: "Int, Valid Item in Middle, Returns True",
+			s:    []int{1, 2, 0, 3, 4},
+			want: true,
+		},
+		{
+			name: "Int, Multiple Valid Items, Returns True",
+			s:    []int{1, 0, 2, 0, 3},
+			want: true,
+		},
+	}
+
 	f := func(n int) bool {
 		return n == 0
 	}
 
-	a := linq.Any(s, f)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			a := linq.Any(testCase.s, f)
 
-	assert.False(t, a)
+			assert.Equal(t, testCase.want, a)
+		})
+	}
 }
 
-func Test_Any_Int_NoValidItems_ReturnsFalse(t *testing.T) {
-	s := []int {
-		1, 2, 3,
-	}
-	f := func(n int) bool {
-		return n == 0
+func Test_Any_Float64(t *testing.T) {
+	testCases := []struct {
+		name string
+		s    []float64
+		want bool
+	}{
+		{
+			name: "Int, Empty Slice, Returns False",
+			s:    make([]float64, 0),
+			want: false,
+		},
+		{
+			name: "Int, No Valid Items, Returns False",
+			s:    []float64{1.0, 2.0, 3.0},
+			want: false,
+		},
+		{
+			name: "Int, Valid Item at Start, Returns True",
+			s:    []float64{0.0, 1.0, 2.0, 3.0},
+			want: true,
+		},
+		{
+			name: "Int, Valid Item at End, Returns True",
+			s:    []float64{1.0, 2.0, 3.0, 0.0},
+			want: true,
+		},
+		{
+			name: "Int, Valid Item in Middle, Returns True",
+			s:    []float64{1.0, 2.0, 0.0, 3.0, 4.0},
+			want: true,
+		},
+		{
+			name: "Int, Multiple Valid Items, Returns True",
+			s:    []float64{1.0, 0.0, 2.0, 0.0, 3.0},
+			want: true,
+		},
 	}
 
-	a := linq.Any(s, f)
+	f := func(n float64) bool {
+		return n == 0.0
+	}
 
-	assert.False(t, a)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			a := linq.Any(testCase.s, f)
+
+			assert.Equal(t, testCase.want, a)
+		})
+	}
 }
 
-func Test_Any_Int_ValidItemAtStart_ReturnsTrue(t *testing.T) {
-	s := []int {
-		0, 1, 2, 3,
-	}
-	f := func(n int) bool {
-		return n == 0
+func Test_Any_String(t *testing.T) {
+	testCases := []struct {
+		name string
+		s    []string
+		want bool
+	}{
+		{
+			name: "Int, Empty Slice, Returns False",
+			s:    make([]string, 0),
+			want: false,
+		},
+		{
+			name: "Int, No Valid Items, Returns False",
+			s:    []string{"a", "b", "c"},
+			want: false,
+		},
+		{
+			name: "Int, Valid Item at Start, Returns True",
+			s:    []string{"", "a", "b", "c"},
+			want: true,
+		},
+		{
+			name: "Int, Valid Item at End, Returns True",
+			s:    []string{"a", "b", "c", ""},
+			want: true,
+		},
+		{
+			name: "Int, Valid Item in Middle, Returns True",
+			s:    []string{"a", "b", "", "c", "d"},
+			want: true,
+		},
+		{
+			name: "Int, Multiple Valid Items, Returns True",
+			s:    []string{"a", "", "b", "", "c"},
+			want: true,
+		},
 	}
 
-	a := linq.Any(s, f)
+	f := func(n string) bool {
+		return n == ""
+	}
 
-	assert.True(t, a)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			a := linq.Any(testCase.s, f)
+
+			assert.Equal(t, testCase.want, a)
+		})
+	}
 }
-
-func Test_Any_Int_ValidItemAtEnd_ReturnsTrue(t *testing.T) {
-	s := []int {
-		1, 2, 3, 0,
-	}
-	f := func(n int) bool {
-		return n == 0
-	}
-
-	a := linq.Any(s, f)
-
-	assert.True(t, a)
-}
-
-func Test_Any_Int_ValidItemInMiddle_ReturnsTrue(t *testing.T) {
-	s := []int {
-		1, 2, 0, 3, 4,
-	}
-	f := func(n int) bool {
-		return n == 0
-	}
-
-	a := linq.Any(s, f)
-
-	assert.True(t, a)
-}
-
-func Test_Any_Int_MultipleValidItems_ReturnsTrue(t *testing.T) {
-	s := []int {
-		1, 0, 2, 0, 3,
-	}
-	f := func(n int) bool {
-		return n == 0
-	}
-
-	a := linq.Any(s, f)
-
-	assert.True(t, a)
-}
-
